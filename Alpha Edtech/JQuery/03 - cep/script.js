639247238659423da8239a0b2f80e7cb7af25a58
httpsbruno <bruno-bbs80@hotmail.com>
2022-02-11 00:57:09 -0300

    atividade jquery

---
+++ Alpha Edtech/JQuery/03 - cep/script.js
@@ -0,0 +1,35 @@
+$(document).ready( function (){
+    $("#box1").hide();
+    $("#box2").hide();
+    //btn Search
+    $("#btn-search").on("click", function(){
+        let cep = $("#cep").val();
+        let validate = true;
+        if(cep.length == 9 && cep.includes("-")){
+            cep = cep.replace("-", "");
+        }else if(cep.length != 8 || isNaN(cep) == true){
+            validate = false;
+            alert("CEP inválido");
+        }
+
+        if (validate){
+            $("#box1").fadeIn();
+            $("#box2").fadeIn();
+            $.ajax({url: `https://cep.awesomeapi.com.br/json/${cep}`})
+                .done( (data) => {
+                    $("#box1").html(`
+                        <p><strong>CEP:</strong> ${data.cep.substring(0,5)}-${data.cep.substring(5,8)}</p>
+                        <p><strong>Address:</strong> ${data.address}</p>
+                        <p><strong>District:</strong> ${data.district}</p>
+                        <p><strong>City:</strong> ${data.city} - <strong>State:</strong> ${data.state}</p>`);
+                            
+                    $("#cep").val("");
+
+                    //map
+                    $("#box2").html(`<iframe width="350" height="300" src="https://maps.google.com/maps?q=${data.lat},${data.lng}&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>`).animate();
+                });
+        }
+
+        }); 
+});
+
