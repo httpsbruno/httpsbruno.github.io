90c8647ff5c69c262bba2ae23aed1e24f430189d
httpsbruno <bruno-bbs80@hotmail.com>
2022-05-17 23:25:10 -0300

    atividades react

---
+++ Alpha Edtech/React/styled/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react'
+import CardList from './components/CardList';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  flex-direction: row;
+  width: 20%;
+  margin: auto;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  padding: 10px;
+  font-size: 30px;
+  border-radius: 20px;
+  text-align: center;
+`
+
+const App =  () => {
+  const [charactersURL, setCharactersURL] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [numEp, setNumEp] = useState(1);
+
+  useEffect(() => {
+    (async () => {
+ 
+        let allUrl = [];
+    
+        const reponse = await fetch(
+        `https://rickandmortyapi.com/api/episode/${numEp}`,
+        );
+        const data = await reponse.json();
+        console.log(data.error);
+        if(data.error || data.info) {
+          setCharactersURL([-1]);
+          setCharacters([-1]);
+        }else {
+          allUrl = allUrl.concat(data.characters);
+          setCharactersURL(allUrl);
+        }
+
+    })();
+  }, [numEp]);
+  
+  //console.log(charactersURL);
+  useEffect(() => {
+    (async () => {
+     if(charactersURL[0] !== -1){
+      let allChars = [];
+      for (let i = 0; i <= charactersURL.length - 1; i++) {
+        const reponse = await fetch(
+          charactersURL[i]
+        );
+        const data = await reponse.json();
+        allChars = allChars.concat(data);
+      }
+      setCharacters(allChars);
+     }
+    })();
+  }, [charactersURL]);
+  //console.log(characters);
+
+  return <div >
+      <Input type="number" placeholder='Digite aqui...' onChange={e =>{
+        setCharacters([])
+        setNumEp(e.target.value)
+      } } value={numEp}></Input>
+      <CardList characters={characters}></CardList>
+  </div>;
+};
+
+export default App
+
